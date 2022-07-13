cc7315630474bfb76c0794d31dd14c421fba742a
Zachary Wisniewski <wisniewskiz@live.com>
2022-07-13 13:49:55 -0500

    Connect MongoDB and connect Mongoose

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+//IMPORTING REQUIRED PACKAGES
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//SETTING UP MONGOOSE AND MONGODB
+mongoose.connect("mongodb://localhost:27017/todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on("error", console.error.bind(console, "Connection error"));
+mongoose.connection.once("open", () => {
+    console.log("Database connected!");
+})
+
+//ROUTES
 app.get('/', (req, res) => {
     res.send('<h1>HELLO WORLD</h1>')
 })
 
+
+//STARTING SERVER
 app.listen(8080, () => {
     console.log('Server listening on port 8080');
 });
